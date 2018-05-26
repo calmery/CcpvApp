f0b49ac534a8738562e295199e5b6d6385537bca
Marei Kikukawa <contact@calmery.me>
2018-05-26 18:28:41 +0900

    Change folder structure

---
+++ src/containers/Top/Top.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import { Logo } from 'components'
+import './Top.css'
+
+export class Top extends Component {
+  render() {
+    return (
+      <div className="Top">
+        <header className="Top-header">
+          <Logo />
+          <h1 className="Top-title">Welcome to React</h1>
+        </header>
+        <p className="Top-intro">
+          Hello World !
+        </p>
+      </div>
+    )
+  }
+}
