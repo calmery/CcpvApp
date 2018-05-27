1870eb473b663ed6b6c4fbf255afa1c66547f01c
Marei Kikukawa <contact@calmery.me>
2018-05-28 02:19:25 +0900

    Use react-router-redux

--- src/containers/Top/Top.js
+++ src/containers/Top/Top.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
-import { connectStore } from 'modules/store'
-
-import { ToDo } from 'containers'
 
 import { Logo } from 'components'
 import './Top.css'
 
-export class TopComponent extends Component {
+export class Top extends Component {
   render() {
     return (
       <div className="Top">
@@ -16,7 +13,6 @@ export class TopComponent extends Component {
           <h1 className="Top-title">Welcome to React</h1>
         </header>
         <p className="Top-intro">Hello World !</p>
-        <ToDo />
         <Button variant="raised" color="primary">
           Hello World
         </Button>
@@ -24,5 +20,3 @@ export class TopComponent extends Component {
     )
   }
 }
-
-export const Top = connectStore(TopComponent)
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,13 @@
-export * from './Top/Top'
-export * from './ToDo/ToDo'
+import { connect } from 'react-redux'
+
+import { Top as TopComponent } from './Top/Top'
+import { ToDo as ToDoComponent } from './ToDo/ToDo'
+
+const connectStore = target => {
+  return connect(state => {
+    return { state }
+  })(target)
+}
+
+export const Top = connectStore(TopComponent)
+export const ToDo = connectStore(ToDoComponent)
