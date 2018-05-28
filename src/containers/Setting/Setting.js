7397c9c1e66b87b74da5c112edb14702b30143a1
s_naoya <sugita6878@gmail.com>
2018-05-28 14:21:27 +0900

    del comment

--- src/containers/Setting/Setting.js
+++ src/containers/Setting/Setting.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
 
 export class Setting extends Component {
   render() {
@@ -8,15 +7,3 @@ export class Setting extends Component {
     )
   }
 }
-//   onAddBtnClicked(e) {
-//     let input = this.refs.input
-//     let text = input.value.trim()
-//     if (!text) return alert('Please input.')
-//     input.value = ''
-//     this.props.dispatch(addText(text))
-//   }
-
-//   onClearBtnClicked(e) {
-//     this.props.dispatch(clearText())
-//   }
-// }
