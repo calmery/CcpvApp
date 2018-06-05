0ca9e0205b80078f4381a7b70bf00afd2dfc843a
s_naoya <sugita6878@gmail.com>
2018-06-05 18:07:10 +0900

    サイドバーとヘッダーにEditを追加

--- src/containers/Edit/Edit.js
+++ src/containers/Edit/Edit.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
+import { setTitle } from 'modules/Title/actions'
+
 export class Edit extends Component {
+  componentDidMount() {
+    this.props.dispatch(setTitle('Edit'))
+  }
+
   render() {
     return <p>Edit</p>
   }
