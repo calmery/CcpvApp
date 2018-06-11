4b4bcc9fc00554607722a355de3536c0cecc3927
tatsu <asagao157@gmail.com>
2018-06-11 17:58:56 +0900

    topページの追加

--- src/containers/Top/Top.js
+++ src/containers/Top/Top.js
@@ -15,16 +15,7 @@ export class Top extends Component {
   render() {
     return (
       <div className="Top">
-        <header className="Top-header">
-          <Logo />
-          <h1 className="Top-title">Welcome to React</h1>
-        </header>
-        <p className="Top-intro">Hello World !</p>
-        <Link to="/todo" style={{ textDecoration: 'none' }}>
-          <Button variant="raised" color="primary">
-            ToDo
-          </Button>
-        </Link>
+        hello!
       </div>
     )
   }
