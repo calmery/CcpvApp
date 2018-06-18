5cf3a3cb66fa2eeda33d48ce2e33443d57345711
tatsu <asagao157@gmail.com>
2018-06-18 17:49:30 +0900

    リストの追加

--- src/containers/Top/Top.js
+++ src/containers/Top/Top.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './Top.css'
 import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
 import { setTitle } from 'modules/Title/actions'
 
@@ -10,12 +13,20 @@ export class Top extends Component {
   }
 
   render() {
-      return (
-        <div className="Top">
-          <Button variant="raised" color="primary">
-            Primary
+    return (
+      <div className="Top">
+        <Button variant="raised" color="primary">
+          Primary
         </Button>
+        <List>
+          <ListItem button>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+        </List>
       </div>
     )
-    }
+  }
 }
