a92ea84e381ed52632dd87d5b60324d8682fcbc6
Fujisan <syou_wfsd8562@yahoo.co.jp>
2018-06-18 17:28:14 +0900

    ボタンを追加

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
 import { setTitle } from 'modules/Title/actions'
-
 export class List extends Component {
   componentDidMount() {
     this.props.dispatch(setTitle('List'))
   }
 
   render() {
-    return <div className="List">hello!</div>
+    return (
+      <div className="List">
+        <Button variant="raised" color="primary">
+          Primary
+        </Button>
+      </div>
+    )
   }
 }
