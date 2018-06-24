4cff11153aeccb8c8743cde541d9530cbf08eb0a
Marei Kikukawa <contact@calmery.me>
2018-06-24 18:08:18 +0900

    Fix

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,7 @@
-export const url = 'http://localhost:8000'
+export const url =
+  process.env.NODE_ENV === 'production' || window.location.hostname.includes('herokuapp.com')
+    ? 'https://ccpv.herokuapp.com'
+    : 'http://localhost:8000'
 
 export const endpoints = {
   authentication: '/authentication'
