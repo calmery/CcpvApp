b5296d2642605c454878c8f40bc27c21e2a9db3e
tatsu <asagao157@gmail.com>
2018-06-25 18:17:55 +0900

    リストを表示する

--- src/containers/Top/Top.js
+++ src/containers/Top/Top.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import './Top.css'
+import axios from 'axios'
 import Button from '@material-ui/core/Button'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 
 export class Top extends Component {
+  constructor(props) {
+   super(props)
+
+   this.state = {
+     data: []
+   }
+ }
   componentDidMount() {
     this.props.setTitle('Top')
+    this.getData()
   }
 
+  async getData() {
+   try {
+     const response = await axios.get('https://quiet-plains-74935.herokuapp.com/list')
+     console.log('Got')
+     this.setState({ data:response.data.lists })
+   } catch(error) {
+     throw error
+   }
+ }
+
   render() {
+    console.log('State', this.state)
     let array = []
 
     for (let i = 0; i < 100; i++) {
@@ -23,9 +43,9 @@ export class Top extends Component {
           Primary
         </Button>
         <List>
-          {array.map(n => (
+          {this.state.data.map(n => (
             <ListItem button>
-              <ListItemText primary={n} />
+              <ListItemText primary={n.title} />
             </ListItem>
           ))}
         </List>
