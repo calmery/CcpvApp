e70d5067df08b07360d26c0dd7adf32266c59c8e
Marei Kikukawa <contact@calmery.me>
2018-07-01 20:56:18 +0900

    Add button to List page

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/Add'
+import { withStyles } from '@material-ui/core/styles'
 import './List.css'
 
 export class List extends Component {
@@ -7,6 +10,14 @@ export class List extends Component {
   }
 
   render() {
-    return <div className="List">hello!</div>
+    const { classes } = this.props
+
+    return (
+      <div className="List">
+        <Button variant="fab" color="primary" aria-label="add" className="button">
+          <AddIcon />
+        </Button>
+      </div>
+    )
   }
 }
