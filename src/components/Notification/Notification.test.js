b773f45ed83706321c5d5101e75d0e97dbf15496
Marei Kikukawa <contact@calmery.me>
2018-07-03 01:44:00 +0900

    Update tests

--- src/components/Notification/Notification.test.js
+++ src/components/Notification/Notification.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Notification } from '../'
+import { Notification } from './Notification'
+import { NotificationDialog } from './NotificationDialog'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Notification />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<NotificationDialog />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/containers/Search/Search.test.js
+++ src/containers/Search/Search.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Search } from '../'
+import { Search } from './Search'
+import { SearchDialog } from './SearchDialog'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Search setTitle={() => {}} />, div)
+  ReactDOM.render(<Search />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<SearchDialog />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
