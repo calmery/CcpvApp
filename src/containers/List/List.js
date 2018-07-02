39558d2d808c3a3fdc264066281e606e90964e81
Marei Kikukawa <contact@calmery.me>
2018-07-02 16:21:25 +0900

    Add search dialog to List component

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import AddIcon from '@material-ui/icons/Add'
 import './List.css'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
 
-import { Header } from 'containers'
+import { Header, Search } from 'containers'
 
 export class List extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false
+    }
+  }
+
   componentDidMount() {
     this.props.setTitle('List')
+  }
+
+  handleClickOpen() {
+    this.setState({ open: true })
+  }
+
+  handleClose() {
+    this.setState({ open: false })
   }
 
   render() {
@@ -19,9 +39,28 @@ export class List extends Component {
           color="primary"
           aria-label="add"
           className="button"
+          onClick={() => this.handleClickOpen()}
         >
           <AddIcon />
         </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={() => this.handleClose()}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">New</DialogTitle>
+          <DialogContent>
+            <Search />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleClose()} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => this.handleClose()} color="primary">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     )
   }
