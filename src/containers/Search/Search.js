3e95c5a9a4938b07152cd0ca228bb78e9dc901e5
Yuya Nagae <yuya.n.0617@icloud.com>
2018-07-09 17:27:14 +0900

    名前記入場所とボタンの配置変更

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -4,13 +4,16 @@ import './Search.css'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
+import axios from 'axios'
+
 export class Search extends Component {
   render() {
     return (
       <div className="Search">
-        <TextField id="検索" label="検索" margin="normal" />
-        <Button variant="raised" color="primary">
-          Primary
+      <TextField id="名前" label="名前" margin="normal" /><br />
+        <TextField id="検索" label="検索" margin="normal" /><br />
+        <Button variant="raised" color="primary" className="button">
+          検索
         </Button>
       </div>
     )
