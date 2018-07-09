b6227de7df84579bfe85e2a115d6c39ce5756fd5
Yuya Nagae <yuya.n.0617@icloud.com>
2018-07-09 18:32:34 +0900

    url変更

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,6 +1,5 @@
 // API の向き先を指定する
 export const url = (() => {
-  return 'https://ccpv-mock.herokuapp.com'
   // Production
   if (process.env.NODE_ENV === 'production') {
     return 'https://ccpv.herokuapp.com'
