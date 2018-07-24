34a9848a57bafd84e213fc64f76a9ee6ab187480
Yuya Nagae <yuya.n.0617@icloud.com>
2018-07-24 11:25:30 +0900

    ページ移動出来るようにした

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -3,43 +3,63 @@ import './Search.css'
 
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import axios from 'axios'
-import {url}from 'constants/url'
+import { url } from 'constants/url'
 export class Search extends Component {
+  constructor(props) {
+    super(props)
 
-constructor(props){
-  super(props)
-
-  this.state ={
-  name:'',
-  query:''
-
+    this.state = {
+      name: '',
+      query: '',
+      loading: false
+    }
   }
-}
 
-search() {
-  axios.post(url + '/list',{
-    name:this.state.name,
-    query:this.state.query
-  }).then(response =>{
-    console.log(response)
-  })
-
-}
-onChangeNameField(event){
-  this.setState({name:event.target.value})
-}
-onChangeQueryField(event){
-  this.setState({query:event.target.value})
-}
+  search() {
+    this.setState({ loading: true})
+    axios
+      .post('https://quiet-plains-74935.herokuapp.com/search', {
+        name: this.state.name,
+        query: this.state.query
+      })
+      .then(response => {
+        window. location.href='/edit?id='+response.data.id
+      })
+  }
+  onChangeNameField(event) {
+    this.setState({ name: event.target.value })
+  }
+  onChangeQueryField(event) {
+    this.setState({ query: event.target.value })
+  }
 
   render() {
     return (
       <div className="Search">
-      <TextField id="名前" label="名前" margin="normal" onChange={c => this.onChangeNameField(c)}/><br />
-        <TextField id="検索" label="検索" margin="normal" onChange={c => this.onChangeQueryField(c)} /><br />
-        <Button variant="raised" color="primary" className="button" onClick={() => this.search()}>
+        <TextField
+          id="名前"
+          label="名前"
+          margin="normal"
+          onChange={c => this.onChangeNameField(c)}
+        />
+        <br />
+        <TextField
+          id="検索"
+          label="検索"
+          margin="normal"
+          onChange={c => this.onChangeQueryField(c)}
+        />
+        <br />
+        <Button
+          variant="raised"
+          color="primary"
+          className="button"
+          onClick={() => this.search()}
+          disabled={this.state.loading}
+        >
           検索
         </Button>
       </div>
