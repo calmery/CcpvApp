394ba3ab8a354bd07016a4ab44bc94debb34a415
Marei Kikukawa <contact@calmery.me>
2018-09-15 21:00:16 +0900

    Use Ionic

--- src/constants/firebase.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = 'AIzaSyDtqRLvPiZCOAqszre9I_V5nEAzV-KlF1c'
-export const authDomain = 'ccpvapp.firebaseapp.com'
-export const databaseURL = 'https://ccpvapp.firebaseio.com'
-export const storageBucket = 'ccpvapp.appspot.com'
--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Header/Header'
-export * from './Edit/Edit'
-export * from './Search/SearchDialog'
-export * from './List/List'
