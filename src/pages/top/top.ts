527efdcf6948580af7eb95e3f45deae6bb191c04
Marei Kikukawa <contact@calmery.me>
2018-09-16 15:46:51 +0900

    Refactored

--- src/pages/top/top.ts
+++ src/pages/top/top.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-top',
+  templateUrl: 'top.html'
 })
-export class HomePage {
+export class TopPage {
 
   constructor(public navCtrl: NavController) {
 
