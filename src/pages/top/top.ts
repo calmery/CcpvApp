535c5c3cd24526eee71eaf5beed508ac593d37b2
Marei Kikukawa <contact@calmery.me>
2018-09-16 15:56:53 +0900

    Add menu module

--- src/pages/top/top.ts
+++ src/pages/top/top.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { SearchPage } from '../search/search';
+
 @Component({
   selector: 'page-top',
   templateUrl: 'top.html'
 })
 export class TopPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController
+  ) {
 
+  }
+
+  moveSearchPage() {
+    this.navCtrl.push(SearchPage);
   }
 
 }
